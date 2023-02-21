aef29d6098911ba718778a0f2a7e24aa85b5b695
JIWON_CHOE <122085449+ch02jw3741@users.noreply.github.com>
2023-02-21 14:37:39 +0900

    Update test.java

--- gitTest/src/gitTest/test.java
+++ gitTest/src/gitTest/test.java
@@ -6,6 +6,8 @@ public class test {
 		System.out.println("안녕하세요.");
 		System.out.println("코드 작성 중입니다...");
 		System.out.println("코드 수정 중입니다.");
+		System.out.println("코드 수정을 완료했습니다.");
+		
 	}
 
 }
