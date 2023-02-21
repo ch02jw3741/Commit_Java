9cfc521333b29a81dd5873ede1a5a443d8fd2d25
Jiwon <codejw3741@gmail.com>
2023-02-21 14:03:37 +0900

    code update1

--- gitTest/src/gitTest/test.java
+++ gitTest/src/gitTest/test.java
@@ -3,7 +3,8 @@ package gitTest;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("안녕하세요.");
+		System.out.println("코드 작성 중입니다...");
 
 	}
 
