7a1e6bc1c186c7375721f536a881ca9c84f461a5
Jiwon <codejw3741@gmail.com>
2023-02-21 14:33:52 +0900

    update2

--- gitTest/src/gitTest/test.java
+++ gitTest/src/gitTest/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요.");
 		System.out.println("코드 작성 중입니다...");
-
+		System.out.println("코드 수정 중입니다.");
 	}
 
 }
